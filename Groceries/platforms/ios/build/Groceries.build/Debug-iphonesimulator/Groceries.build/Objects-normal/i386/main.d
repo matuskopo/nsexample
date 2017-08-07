e8c94611f86c96e12ce2ce24b0fce8c42bef17f6
Matus <matus@vinidev.com>
2017-08-07 18:04:15 +0200

    gitignore added

--- Groceries/platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
+++ Groceries/platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -1,4 +1,5 @@
 dependencies: \
+  /Users/matus/Work/NativeScript/nsexample/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
   /Users/matus/Work/NativeScript/nsexample/Groceries/platforms/ios/internal/main.m \
   /Users/matus/Work/NativeScript/nsexample/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
   /Users/matus/Work/NativeScript/nsexample/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
