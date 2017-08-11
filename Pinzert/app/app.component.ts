cac35b67f89d85a3bc16a71315335f9fb14a12a2
Matus <matus@vinidev.com>
2017-08-11 10:32:59 +0200

    reinstalled Pinzert

--- Pinzert/app/app.component.ts
+++ Pinzert/app/app.component.ts
@@ -1,58 +1,12 @@
 import { Component } from "@angular/core";
-import { User } from "./shared/user/user";
-import { UserService } from "./shared/user/user.service";
-// import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
-  selector: "pinzert-app",
-  providers: [UserService],
-  templateUrl: "pages/login/login.html",
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <!-- Your UI components go here -->
+  `
 })
 export class AppComponent {
-  user: User;
-  isLoggingIn = true;
-
-  // constructor(private userService: UserService, private afAuth: AngularFireAuth) {
-  constructor(private userService: UserService) {
-    this.user = new User();
-
-    // afAuth.auth.onAuthStateChanged(function(user) {
-    //   if (user) {
-    //     // User is signed in.
-    //     var displayName = user.displayName;
-    //     var email = user.email;
-    //     var emailVerified = user.emailVerified;
-    //     var photoURL = user.photoURL;
-    //     var isAnonymous = user.isAnonymous;
-    //     var uid = user.uid;
-    //     var providerData = user.providerData;
-        
-    //     console.log("Logged in");
-    //   } else {
-    //     // User is signed out.
-    //     console.log("Not logged in");
-    //   }
-    // });
-  }
-
-  submit() {
-    if (this.isLoggingIn) {
-      this.login();
-    } else {
-      this.signUp();
-    }
-  }
-
-  login() {
-    // TODO: Define
-  }
-  
-  signUp() {
-    this.userService.register(this.user);
-  }
-
-  toggleDisplay() {
-    this.isLoggingIn = !this.isLoggingIn;
-  }
+  // Your TypeScript logic goes here
 }
--- Pinzert/app/app.module.ts
+++ Pinzert/app/app.module.ts
@@ -1,21 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-// import { AngularFireModule } from 'angularfire2';
-// import { AngularFireAuthModule } from 'angularfire2/auth';
-import { Config } from "./shared/config";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [
-    NativeScriptModule,
-    NativeScriptFormsModule,
-    // AngularFireModule.initializeApp(Config.firebase),
-    // AngularFireAuthModule
-  ],
+  imports: [NativeScriptModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
