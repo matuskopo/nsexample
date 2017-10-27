5dcc1211aee22f29a4e18b6c16d78059208b69b4
Matus <matus@vinidev.com>
2017-10-27 13:51:32 +0200

    new package added

--- Pinzert/app/app.module.ts
+++ Pinzert/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {Config} from './shared/config'
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule],
+  imports: [
+    NativeScriptModule,
+    AngularFireModule.initializeApp(Config.firebase),
+    AngularFireAuthModule
+  ],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
