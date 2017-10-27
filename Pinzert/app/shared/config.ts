28d57148071965193bb2f1db7ade56c227598634
Matus <matus@vinidev.com>
2017-10-27 13:55:45 +0200

    changed config

---
+++ Pinzert/app/shared/config.ts
@@ -0,0 +1,10 @@
+export class Config {
+  static firebase = {
+    apiKey: "AIzaSyAOYh2FoPwDYbXCpvIT8ETH1hep4hG-1Vw",
+    authDomain: "nsgroceries-881ae.firebaseapp.com",
+    databaseURL: "https://nsgroceries-881ae.firebaseio.com",
+    projectId: "nsgroceries-881ae",
+    storageBucket: "nsgroceries-881ae.appspot.com",
+    messagingSenderId: "117542409211"
+  };
+}
